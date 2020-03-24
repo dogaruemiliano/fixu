aab5cdcb2d3064e9b19d4b32f4182fd728f6d4e3
Emiliano Dogaru <dogaru.emiliano@gmail.com>
2020-03-24 12:43:28 +0100

    User seed and added 'faker' >> Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "*" * 80
+puts "Seeding started"
+puts
+
+puts "-" * 80
+puts "Creating users"
+puts "-" * 80
+users_data = [
+  {
+    email: "emi@gmail.com",
+    password: "123456",
+    first_name: "Emiliano",
+    last_name: "Dogaru",
+    phone_number: Faker::PhoneNumber.phone_number
+  },
+
+  {
+    email: "sang@gmail.com",
+    password: "123456",
+    first_name: "Sang-Soo",
+    last_name: "Ra",
+    phone_number: Faker::PhoneNumber.phone_number
+  },
+
+  {
+    email: "leon@gmail.com",
+    password: "123456",
+    first_name: "Leon",
+    last_name: "Ellerbrock",
+    phone_number: Faker::PhoneNumber.phone_number
+  },
+
+  {
+    email: "Abder@gmail.com",
+    password: "123456",
+    first_name: "Abderrahim",
+    last_name: "Nadifi",
+    phone_number: Faker::PhoneNumber.phone_number
+  },
+]
+
+users_data.each do |user_data|
+  user = User.create!(user_data)
+  puts "Created user with email:#{user.email}"
+end
+puts "-" * 80
+puts "Done creating users"
+puts "-" * 80
+
+puts
+puts "Seeding ended"
+puts "*" * 80
