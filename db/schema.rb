73acf6f5a489f89a6ead4ece24322de99e62f1c2
Abder Nadifi <nabder@naditel.com>
2020-03-24 18:58:45 +0100

    change content to string in problems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_153700) do
+ActiveRecord::Schema.define(version: 2020_03_24_175652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_153700) do
   end
 
   create_table "problems", force: :cascade do |t|
-    t.text "content"
+    t.string "content"
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
