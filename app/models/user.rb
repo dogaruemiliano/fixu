3c9f2ae0ec514dfc85cd785845c554a3b820214d
Emiliano Dogaru <dogaru.emiliano@gmail.com>
2020-03-24 13:54:08 +0100

    Added params_sanitizer and REGEX validation for email and phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, presence: true, format: { with: /.+@.+\..+/ }
   validates :first_name, presence: true, length: { minimum: 2, maximum: 20 }
   validates :last_name, presence: true, length: { minimum: 2, maximum: 20 }
   validates :address, presence: true, length: {minimum: 7, maximum: 40}
+  validates :phone_number, presence: true, format: { with: /\d+/}
+
   # has_one :fixers # is this correct
 end
