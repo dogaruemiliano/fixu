1b0b780f74e9f9a99189e5b45c1e4eeb7cdc59f2
Emiliano Dogaru <dogaru.emiliano@gmail.com>
2020-03-24 20:46:57 +0100

    Deleted line of code from conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     resources :problems, only: [:new, :create, :index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   resources :users, only: [:show] do
     resources :fixers, only: [:new, :create, :destroy]
   end
