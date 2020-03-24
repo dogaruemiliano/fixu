b801cc08a3379df7987889e2f20201a5a7ad04f3
Abder Nadifi <nabder@naditel.com>
2020-03-24 14:38:01 +0100

    creating categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,8 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
   def index
+    @categories = Category.all
   end
 
   def show
@@ -8,15 +11,39 @@ class CategoriesController < ApplicationController
   def edit
   end
 
-  def update
-  end
-
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def update
+    @category.update(category_params)
+    redirect_to categories_path
+
+    # Will raise ActiveModel::ForbiddenAttributesError
   end
 
   def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
