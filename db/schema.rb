77355867e043a734c63a14df01a418f404b7f69d
Emiliano Dogaru <dogaru.emiliano@gmail.com>
2020-03-26 12:33:46 +0100

    Added comment field on Appointments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_160819) do
+ActiveRecord::Schema.define(version: 2020_03_26_113029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_03_25_160819) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "comment"
     t.index ["fixer_id"], name: "index_appointments_on_fixer_id"
     t.index ["problem_id"], name: "index_appointments_on_problem_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
@@ -72,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_03_25_160819) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "duration"
     t.index ["category_id"], name: "index_problems_on_category_id"
   end
 
