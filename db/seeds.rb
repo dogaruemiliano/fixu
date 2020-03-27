deda9e2f607b7d376e856b13dfc667ae62c39ca6
Emiliano Dogaru <dogaru.emiliano@gmail.com>
2020-03-27 16:49:33 +0100

    Changed seed and deleted part from users/show

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,14 @@ users_data = [
     address: "Barcelona, Simo Oller 2",
     phone_number: Faker::PhoneNumber.phone_number
   },
+  {
+    email: "abder@gmail.com",
+    password: "123456",
+    first_name: "Abderrahim",
+    last_name: "Nadifi",
+    address: "Barcelona, Grassot 101",
+    phone_number: Faker::PhoneNumber.phone_number
+  },
 
   {
     email: "leon@gmail.com",
@@ -27,16 +35,8 @@ users_data = [
     last_name: "Ellerbrock",
     address: "Barcelona, Calvet 58",
     phone_number: Faker::PhoneNumber.phone_number
-  },
-
-  {
-    email: "Abder@gmail.com",
-    password: "123456",
-    first_name: "Abderrahim",
-    last_name: "Nadifi",
-    address: "Barcelona, Grassot 101",
-    phone_number: Faker::PhoneNumber.phone_number
   }
+
 ]
 
 fixers_data = [
@@ -58,62 +58,6 @@ fixers_data = [
     unit_price: 15
   }
 ]
-
-puts "*" * 60
-puts "Seeding started"
-puts "=" * 60
-
-puts "-" * 60
-puts "Destroying models"
-puts "-" * 30
-
-User.destroy_all
-puts "Destroyed all User models"
-
-Category.destroy_all
-puts "Destroyed all Category models"
-
-Appointment.destroy_all
-puts "Destroyed all Appointment models"
-
-puts "-" * 30
-puts "Destroyed all previous models"
-puts "-" * 60
-
-puts "=" * 60
-
-puts "-" * 60
-puts "Creating users"
-puts "-" * 30
-
-users_data.each do |user_data|
-  user = User.create!(user_data)
-  puts "Created user with email: #{user.email}"
-end
-puts "-" * 30
-puts "Done creating users"
-puts "-" * 60
-puts "=" * 60
-puts "-" * 60
-puts "Creating fixers"
-puts "-" * 30
-
-data_index = 0 # Helper to iterate throgh the fixers_data
-User.first(3).each do |user|
-  fixer = Fixer.new(fixers_data[data_index])
-  fixer.user = user
-  fixer.save!
-  data_index += 1
-  puts "\ttCreated fixer from user with email: #{fixer.user.email}"
-end
-puts "-" * 30
-puts "Done creating fixer"
-puts "-" * 60
-puts "=" * 60
-puts "-" * 60
-
-puts "Creating categories"
-puts "-" * 30
 
 categories_data = [
   {
@@ -251,6 +195,63 @@ categories_data = [
   #   ]
   # }
 ]
+
+puts "*" * 60
+puts "Seeding started"
+puts "=" * 60
+
+puts "-" * 60
+puts "Destroying models"
+puts "-" * 30
+
+User.destroy_all
+puts "Destroyed all User models"
+
+Category.destroy_all
+puts "Destroyed all Category models"
+
+Appointment.destroy_all
+puts "Destroyed all Appointment models"
+
+puts "-" * 30
+puts "Destroyed all previous models"
+puts "-" * 60
+
+puts "=" * 60
+
+puts "-" * 60
+puts "Creating users"
+puts "-" * 30
+
+users_data.each do |user_data|
+  user = User.create!(user_data)
+  puts "Created user with email: #{user.email}"
+end
+puts "-" * 30
+puts "Done creating users"
+puts "-" * 60
+puts "=" * 60
+puts "-" * 60
+puts "Creating fixers"
+puts "-" * 30
+
+data_index = 0 # Helper to iterate throgh the fixers_data
+User.first(3).each do |user|
+  fixer = Fixer.new(fixers_data[data_index])
+  fixer.user = user
+  fixer.save!
+  data_index += 1
+  puts "\ttCreated fixer from user with email: #{fixer.user.email}"
+end
+puts "-" * 30
+puts "Done creating fixer"
+puts "-" * 60
+puts "=" * 60
+puts "-" * 60
+
+puts "Creating categories"
+puts "-" * 30
+
 
 categories_data.each do |category_data|
   category = Category.create(name: category_data[:name])
