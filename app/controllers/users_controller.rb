8669a52a6857870c4b6ec4bd0b987e6f1533f49e
Emiliano Dogaru <dogaru.emiliano@gmail.com>
2020-03-27 15:24:24 +0100

    Fixed bug in UserController#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @appointments = current_user.appointments
-    @upcoming_appointments = @appointments.select{|appointment| appointment.status == “confirmed”}
+    @upcoming_appointments = @appointments.select{|appointment| appointment.status == "confirmed"}
   end
 end
